972ac2e601c6fbc3154d6ae84794e12d7c4a0813
Jeremias ramirez <jereramirez499@gmail.com>
2019-10-07 10:55:17 -0400

    Agregando estilos y datos externos

--- index.php
+++ index.php
@@ -4,12 +4,45 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Me</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="public/css/main.css">
+	<link rel="stylesheet" href="public/css/lib/jeremias-lib/jeremias-lib.css">
+	<link rel="stylesheet" href="public/css/lib/animate/animate.css">
 </head>
 <body>
 	<script src="node_modules/angular/angular.min.js"></script>
 	<script src="public/js/controller/controller-me.js"></script>
 
+<div ng-controller="showInformation as info" class="container">
+	
+	<h1 class="title__app">My location</h1>
+
+
+	<div class="btns">
+
+		<button class="btn--rm min" ng-click="btn_hide()" ng-hide="btn_hide_me">Ocultar</button>
+		<p></p>
+		<button class="btn--yes min" ng-hide="btn_show_me" ng-click="btn_show()">Mostrar</button>
+		
+	</div>
+
+	<div ng-hide="data_hide" class="containerData" class="animated fadeInUp">
+
+		
+		<p><span>{{"Pais:" | uppercase}} </span>{{countryName}}</p>
+		<p><span>{{"IP:" | uppercase}}</span> {{myip}}</p>
+		<p><span>{{"Currency:" | uppercase}}</span> {{curr}}</p>
+		<p><span>{{"Currency symbol:" | uppercase}}</span> {{currSymbol}}</p>
+
+
+	</div>
+
+
+
+
+
+
+
+</div>
 
 
 
--- public/js/controller/controller-me.js
+++ public/js/controller/controller-me.js
@@ -2,7 +2,57 @@ var me = angular.module("me", []);
 
 
 
-me.controller("showInformation", ["$scope", "$http", ()=>{
+me.controller("showInformation", ["$scope", "$http", ($scope, $http)=>{
+
+
+/* 
+
+	request to extern app data(ajax)
+
+*/
+
+	$scope.countryName = '' ;
+	$scope.myip = '' ;
+	$scope.curr = '';
+	$scope.currSymbol = '';
+
+
+	$http({
+		method: "get",
+		url: "http://www.geoplugin.net/json.gp/"
+	}).then( (resp)=>{
+
+		$scope.countryName = resp.data.geoplugin_countryName; 
+		$scope.myip = resp.data.geoplugin_request;
+		$scope.curr = resp.data.geoplugin_currencyCode;
+		$scope.currSymbol = resp.data.geoplugin_currencySymbol;
+	 
+
+	})
+
+
+	$scope.data_hide = 0;
+	$scope.btn_hide_me = 0;
+	$scope.btn_show_me = 1;
+
+
+	$scope.btn_show = function(){
+
+		$scope.data_hide = 0;
+		$scope.btn_show_me = 1;
+		$scope.btn_hide_me = 0;
+
+	}
+
+	$scope.btn_hide = function(){
+
+		$scope.data_hide = 1;
+
+		$scope.btn_show_me = 0;
+		$scope.btn_hide_me = 1;
+	}
+
+
 
 
 
