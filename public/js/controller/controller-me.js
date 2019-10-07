4fa57c81e2c8bab0f0a8360819ddccf2c690dee0
Jeremias ramirez <jereramirez499@gmail.com>
2019-10-07 12:09:58 -0400

    Add .giignore

--- public/js/controller/controller-me.js
+++ public/js/controller/controller-me.js
@@ -25,18 +25,27 @@ me.controller("showInformation", ["$scope", "$http", ($scope, $http)=>{
 	$scope.myip = '' ;
 	$scope.curr = '';
 	$scope.currSymbol = '';
+	$scope.dollar = '';
+	$scope.city = '';
+	$scope.long = '';
+	$scope.lat = '';
 
 
 	$http({
 		method: "get",
 		url: "http://www.geoplugin.net/json.gp/"
-	}).then( (resp)=>{
+	}).then(
 
-		$scope.countryName = resp.data.geoplugin_countryName; 
-		$scope.myip = resp.data.geoplugin_request;
-		$scope.curr = resp.data.geoplugin_currencyCode;
-		$scope.currSymbol = resp.data.geoplugin_currencySymbol;
-	 
+		(resp)=>{
+
+			$scope.countryName = resp.data.geoplugin_countryName; 
+			$scope.myip = resp.data.geoplugin_request;
+			$scope.curr = resp.data.geoplugin_currencyCode;
+			$scope.currSymbol = resp.data.geoplugin_currencySymbol;
+			$scope.city = resp.data.geoplugin_timezone;
+			$scope.dollar = resp.data.geoplugin_currencyConverter;
+			$scope.long = resp.data.geoplugin_longitude;
+			$scope.lat = resp.data.geoplugin_latitude;
 
 	})
 
